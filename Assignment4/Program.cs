2214608b5f8489ca35ff2e135306dfea276bfdaa
visualstudio <visualstudio@122-PC05>
2019-04-05 19:07:08 -0400

    7:07 copmmit

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -23,6 +23,40 @@ this.Read TextFiles();
 
 // Read file using StreamReader. Read file line by line
 using (StreamReader file = new StreamReader("C:/area51/beowulf.txt"))
+    {
+    int counter 0;
+string In
+
+while ((in = file.ReadLine()) != null)
+
+Console.WriteLine(1n );
+Beowulf.Add(1n)
+
+file.close();
+Console.WriteLine($"File has {contour} lines.");
         }
-    }
 }
+
+
+public int Find Number Of Blank Spaces(string line)
+
+// https://stackoverflow.com/questions/17812566/count-words-and-spaces-in-s
+
+int countletters = 0;
+int countSpaces = 0;
+
+foreach (char c in line)
+
+if (char.IsLetter(c)) { count letters c++;}
+if (char.IsWhiteSpace()) { countSpaces++; }
+
+return countSpaces;
+
+}
+
+    }
+
+}
+    }
+
+
