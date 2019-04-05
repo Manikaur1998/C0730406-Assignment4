79290928830efe922080b996d14e8d4bd653051b
visualstudio <visualstudio@122-PC05>
2019-04-05 18:31:21 -0400

    6:30 commit

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -8,8 +8,21 @@ namespace Assignment4
 {
     class Program
     {
+        //Manpreet kaur(730406)
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+
+            public void Run()
+
+this.Read TextFiles();
+        }
+
+        public void Read TextFiles()
+
+// Read file using StreamReader. Read file line by line
+using (StreamReader file = new StreamReader("C:/area51/beowulf.txt"))
         }
     }
 }
