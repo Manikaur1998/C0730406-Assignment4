89a3c021a2a7f2e7519f32cfbdafcf6324daea03
visualstudio <visualstudio@122-PC05>
2019-04-05 20:36:11 -0400

    Section B

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -33,24 +33,45 @@ namespace Assignment4
             using (StreamReader file = new StreamReader("U:\\Users\\730406\\manni bithday\\Beowulf.txt"))
             {
 
-                int counter = 0;
-                string ln;
 
-                while ((ln = file.ReadLine()) != null)
+
+
+                int words = 0;
+                string delim = " ,.";
+                string[] fields = null;
+                string line = null;
+
+                while (!file.EndOfStream)
                 {
-                    Console.WriteLine(ln);
-                    Beowulf.Add(ln);
+                    line = file.ReadLine();//each time you read a line you should split it into the words
+                    line.Trim();
+                    fields = line.Split(delim.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    words += fields.Length; //and just add how many of them there is
                 }
+
+
+
+                Console.WriteLine("The word count is {0}", words);
+
+
                 file.Close();
-
-                counter = File.ReadLines("U:\\Users\\730406\\manni bithday\\Beowulf.txt").Count();
-
-                Console.WriteLine("\n Number of lines : " + counter);
-
-
 
             }
         }
-       
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+
+                if (char.IsWhiteSpace(c)) { countletters++; }
+            }
+            return countSpaces;
+
+        }
+
     }
 }
